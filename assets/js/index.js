9618e35521cb0c0723aa805c617e7ba3f47268aa
Clément <japanclems287@gmail.com>
2021-10-06 21:40:13 +0200

    📝, 🔨, 💬, 🍱 (data) : Add new entry to experiences

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,31 @@
 // Fonction de vérification de la parité d'un nombre.
 function estPair(nombre) {
-    if (nombre % 2 == 0)
-        return true;
-    else
-        return false;
+	if (nombre % 2 == 0)
+		return true;
+	else
+		return false;
 }
 
 // Fonction d'ajout de Rule CSS.
 function insertCSS(selecteur, propriete) {
-    styleS.insertRule(selecteur + '{' + propriete + '}', 1);
+	styleS.insertRule(selecteur + '{' + propriete + '}', 1);
 }
 
-// Fonction de positionnement des ornements.
-function aligneDeco(n, section, cercles, vLignes) { 
-    let totalHauteur = 26;
-    let cerclesCompteur = 0;
-    let vLignesCompteur = 1;
 
-	for (element = 0; element <= n; element++) {
+/**==============================================
+ **              aligneDeco
+ *?  Fonction de positionnement des ornements.
+ *@param numberOfDecorations int  
+ *@param section NodeList  
+ *@param cercles Array  
+ *@param vLignes Array  
+ *=============================================**/
+function aligneDeco(numberOfDecorations, section, cercles, vLignes) {
+	let totalHauteur = 26,
+		cerclesCompteur = 0,
+		vLignesCompteur = 1;
+
+	for (element = 0; element <= numberOfDecorations; element++) {
 		if (estPair(element) == true) {
 			if (element === 0) {
 				insertCSS(cercles[0], `top: ${totalHauteur}px`);
@@ -26,12 +34,12 @@ function aligneDeco(n, section, cercles, vLignes) {
 			}
 			cerclesCompteur++;
 		} else {
-			if (element === n) {
-                let hauteurLigne = section[element - vLignesCompteur].offsetHeight - 20 - 6 - 5;
+			if (element === numberOfDecorations) {
+				let hauteurLigne = section[element - vLignesCompteur].offsetHeight - 20 - 6 - 5;
 				insertCSS(vLignes, `top: ${totalHauteur + 5}px`);
 				insertCSS(vLignes, `height: ${hauteurLigne}px`);
 			} else {
-                let hauteurLigne = section[element - vLignesCompteur].offsetHeight;
+				let hauteurLigne = section[element - vLignesCompteur].offsetHeight;
 				insertCSS(vLignes[element - vLignesCompteur], `top: ${totalHauteur + 5}px`);
 				insertCSS(vLignes[element - vLignesCompteur], `height: ${hauteurLigne}px`);
 				totalHauteur += hauteurLigne;
@@ -46,8 +54,8 @@ function aligneDeco(n, section, cercles, vLignes) {
 let styleS = document.styleSheets[0];
 
 // Arrays contenants les sélecteurs CSS des cercles et lignes verticales d'ornement de la "section" #ex_professionnelles.
-let cerclesPro = ["#ex_professionnelles > div:last-child > div:first-child::before", "#ex_professionnelles > div:last-child > div:nth-child(2)::before", "#ex_professionnelles > div:last-child > div:last-child::before"];
-let vLignesPro = ["#ex_professionnelles > div:last-child > div:first-child::after", "#ex_professionnelles > div:last-child > div:nth-child(2)::after", "#ex_professionnelles > div:last-child > div:last-child::after"];
+let cerclesPro = ["#ex_professionnelles > div:last-child > div:first-child::before", "#ex_professionnelles > div:last-child > div:nth-child(2)::before", "#ex_professionnelles > div:last-child > div:nth-child(3)::before", "#ex_professionnelles > div:last-child > div:last-child::before"];
+let vLignesPro = ["#ex_professionnelles > div:last-child > div:first-child::after", "#ex_professionnelles > div:last-child > div:nth-child(2)::after", "#ex_professionnelles > div:last-child > div:nth-child(3)::after", "#ex_professionnelles > div:last-child > div:last-child::after"];
 // Arrays contenants le sélecteur CSS du cercle et de la ligne verticale d'ornement de la "section" #education.
 let cerclesEdu = ["#education > div:last-child > div:first-child::before", "#education > div:last-child > div:last-child::before"];
 let vLignesEdu = ["#education > div:last-child > div:first-child::after", "#education > div:last-child > div:last-child::after"];
@@ -60,14 +68,14 @@ let section = [document.querySelectorAll('#ex_professionnelles > div:last-child 
 
 function resize() {
 
-    aligneDeco(5, section[0], cerclesPro, vLignesPro);
-    aligneDeco(3, section[1], cerclesEdu, vLignesEdu);
+	aligneDeco(7, section[0], cerclesPro, vLignesPro);
+	aligneDeco(3, section[1], cerclesEdu, vLignesEdu);
 
 
-    // Positionne les lignes horizontales d'ornement de la "section" #hobbies.
-    let icone = document.querySelectorAll("#hobbies > ul > li");
-    // Récupère la différence entre la deuxième et la première li en partant de gauche. Cette différence est l'espacement entre les deux éléments.
-    let x = icone[1].offsetLeft - icone[0].offsetLeft - 80 + 2;
-    insertCSS(hLignes, `right: -${x}px`);
-    insertCSS(hLignes, `width: ${x}px`);
+	// Positionne les lignes horizontales d'ornement de la "section" #hobbies.
+	let icone = document.querySelectorAll("#hobbies > ul > li");
+	// Récupère la différence entre la deuxième et la première li en partant de gauche. Cette différence est l'espacement entre les deux éléments.
+	let x = icone[1].offsetLeft - icone[0].offsetLeft - 80 + 2;
+	insertCSS(hLignes, `right: -${x}px`);
+	insertCSS(hLignes, `width: ${x}px`);
 }
